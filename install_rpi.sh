86990749fa0d48076c44e48757d137eaca219558
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-19 18:23:17 -0400

    Add more dependencies

--- install_rpi.sh
+++ install_rpi.sh
@@ -1,3 +1,5 @@
 sudo apt -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
 
 sudo apt -y install qt4-dev-tools libatlas-base-dev libhdf5-103 
+
+sudo apt -y install at-spi2-core
