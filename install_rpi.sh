45c1bc20db2a281d98f9bf8f034b68aec72cf5e6
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-19 17:50:28 -0400

    Add dependency install script for raspberry-pi

---
+++ install_rpi.sh
@@ -0,0 +1,3 @@
+sudo apt -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+
+sudo apt -y install qt4-dev-tools libatlas-base-dev libhdf5-103 
