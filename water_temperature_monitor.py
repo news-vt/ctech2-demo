2be82787d1a8c5e02d92196e84d7fc5dd85be585
Alexander DeRieux <alexander.derieux@gmail.com>
2022-06-23 23:37:12 +0100

    Now reads temperature from sensor

--- water_temperature_monitor.py
+++ water_temperature_monitor.py
@@ -1,3 +1,11 @@
+"""Water Temperature Sensor (ds18b20)
+
+This script interacts with Blynk.io to upload water temperature values.
+
+The temperature sensor (ds18b20) communicates over a 1-wire interface.
+By default, this is GPIO 4 on the Raspberry Pi.
+"""
+
 from __future__ import annotations
 import BlynkLib as bl
 import BlynkTimer as bt
@@ -6,11 +14,24 @@ import sys
 import yaml
 from datetime import datetime
 import random
+import time
+import glob
 
 
-def eprint(*args, **kwargs):
-    """Print to standard error."""
-    print(*args, file=sys.stderr, **kwargs)
+base_dir = Path('/sys/bus/w1/devices')
+device_folders = glob.glob(str(base_dir/'28*'))
+if len(device_folders) == 0:
+    print('No 1-wire devices found')
+    sys.exit(1)
+elif len(device_folders) > 1:
+    print('Multiple 1-wire devices found:')
+    for df in device_folders:
+        print(df)
+
+# Isolate 1-wire device to use.
+device_folder = Path(device_folders[0])
+device_file = device_folder/'w1_slave'
+print(f'Using 1-wire device: {device_file}')
 
 
 # Load configuration info.
@@ -20,7 +41,7 @@ if blynk_config_path.exists():
     with open(blynk_config_path, 'r') as f:
         blynk_config = yaml.safe_load(f)
 else:
-    eprint(f"YAML config file not found")
+    print(f"YAML config file not found")
     sys.exit(1)
 
 
@@ -66,15 +87,29 @@ def device_off():
     blynk.virtual_write(BLYNK_PINS['led_red'], 1)
 
 
-def read_temperature() -> float:
-    return random.uniform(-100, 100)
+def read_temperature_raw(device: Path):
+    with open(device, 'r') as f:
+        lines = f.readlines()
+    return lines
+ 
+def read_temperature(device: Path) -> tuple[float, float]:
+    lines = read_temperature_raw(device)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temperature_raw(device)
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
 
 def blynk_set_temperature_value():
     update_time = datetime.now()
-    temperature_value = read_temperature()
-    blynk.virtual_write(BLYNK_PINS['temperature'], temperature_value)
+    temperature_value = read_temperature(device_file)
+    blynk.virtual_write(BLYNK_PINS['temperature'], temperature_value[0])
     blynk.virtual_write(BLYNK_PINS['time'], f"{update_time}")
-    print(f"Temperature Update: {temperature_value} ºF ({update_time})")
+    print(f"Temperature Update: {temperature_value[0]} ºC / {temperature_value[1]} ºF ({update_time})")
 
 
 # Add Timers
